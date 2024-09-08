402315b5687511cb0cdd473e6f20836f0077f0cb
master-utsav <utsavjaiswal890@gmail.com>
2024-09-08 22:17:08 +0530

    INIT

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,7 +4,7 @@ function authenticateToken(req, res, next) {
     const token = req.headers['token'];
     if (!token) return res.status(401).json({ message: 'Access denied, no token provided' });
 
-    jwt.verify(token, SECRET_KEY, (err, user) => {
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
         if (err) return res.status(403).json({ message: 'Invalid token' });
         req.user = user;
         next();
